c9c5f3ee189afef1aed449f0891cd26d9fa669be
David Davydov <kctrlv@gmail.com>
2016-08-08 16:53:46 -0600

    I4: guess limiting implemented

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,23 +1,37 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-
+@@guesses = 5
+@@SECRET_NUM = rand(100)
 
 get '/' do
   guess = params['guess']
-  message = check_guess(guess) if guess
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  check_guess(guess.to_i) if guess
+  data = {:message => @msg, :bg_color => @color, :num_guesses => @@guesses}
+  erb :index, :locals => data
 end
 
 def check_guess(guess)
-  mark = guess.to_i - SECRET_NUMBER
-  if mark > 0
-    mark.abs > 5 ? 'Way too high!' : 'Too high!'
-  elsif mark < 0
-    mark.abs > 5 ? 'Way too low!' : 'Too low!'
+  return nil unless guess
+  @@guesses -= 1
+  if guess == @@SECRET_NUM
+    @color = 'springgreen'
+    @msg = "You got it right! The secret number is #{@@SECRET_NUM}. Please play again."
+    return reset
+  elsif (guess - @@SECRET_NUM).abs <= 5
+    @color = 'lightpink'
+    @msg = guess > @@SECRET_NUM ? "Too high!" : "Too low"
   else
-    "You got it right! The secret number is #{SECRET_NUMBER}"
+    @color = 'red'
+    @msg = guess > @@SECRET_NUM ? "Way too high!" : "Way too low!"
+  end
+  if @@guesses == 0
+    @msg = "You lost. The number has been reset. Care to try again?"
+    reset
+  end
 
+  def reset
+    @@guesses = 5
+    @@SECRET_NUM = rand(100)
   end
 end
